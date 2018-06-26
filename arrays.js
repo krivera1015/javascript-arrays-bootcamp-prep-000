8b193ab6b4d67a93ca50cd1780d67989bff90f0c
krivera1015 <krivera1015@gmail.com>
2018-06-26 01:29:32 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray(array, element) {
+  return [element,...array]
+};
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array;
+};
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+  return newArray;
+};
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+    return array;
+};
