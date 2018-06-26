39fb68f4d9cd887c057397805f60cff780fe7f59
krivera1015 <krivera1015@gmail.com>
2018-06-26 02:28:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.splice(1)
+  array = array.splice(1)
   return array;
 }
